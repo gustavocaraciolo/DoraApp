7a4417f69a014e5d7b780f7dd9a9310045fd1cd3
Gustavo Caraciolo <gustavocaraciolo@gmail.com>
2017-11-23 01:09:04 +0100

    Initial application generated by JHipster-4.10.2

---
+++ src/main/java/com/dora/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.dora.repository.search;
+
+import com.dora.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,57 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { DoraAppSharedModule, UserRouteAccessService } from './shared';
+import { DoraAppHomeModule } from './home/home.module';
+import { DoraAppAdminModule } from './admin/admin.module';
+import { DoraAppAccountModule } from './account/account.module';
+import { DoraAppEntityModule } from './entities/entity.module';
+
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+
+import {
+    JhiMainComponent,
+    LayoutRoutingModule,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        DoraAppSharedModule,
+        DoraAppHomeModule,
+        DoraAppAdminModule,
+        DoraAppAccountModule,
+        DoraAppEntityModule,
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class DoraAppAppModule {}
